3ac5dcdd3e886553ac348c4c4cd3f114820a78fe
meokullu <meokullu@gmail.com>
2024-09-01 17:24:46 +0300

    v1.12.1

--- ColorizeNumber/src/RandomColorLimit.cs
+++ ColorizeNumber/src/RandomColorLimit.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ColorizeNumber
+﻿namespace ColorizeNumber
 {
     public partial class ColorizeNumber
     {
